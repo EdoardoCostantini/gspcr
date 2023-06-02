f58bb1cdd4ad016fa6c1dd058c3a11520742df5d
Edo <38205413+EdoardoCostantini@users.noreply.github.com>
2023-06-02 16:18:21 +0200

    docs: explain 1-se rule in great detail

--- R/cv_choose.R
+++ R/cv_choose.R
@@ -8,10 +8,18 @@
 #' @param K numeric vector of length 1 storing the number of folds for the K-fold cross-validation procedure
 #' @param fit_measure character vector of length 1 indicating the type of fit measure to be used in the to cross-validation procedure
 #' @details
-#' Given a matrix of \eqn{npcs \times nthrs}, returns the best choice based on the type of fit measure (best overall and 1se rule versions)
+#' Given a matrix of \eqn{npcs \times nthrs}, returns the best choice based on the type of fit measure (best overall and 1se rule versions.)
+#' This function returns as solutions:
+#' - \code{default}: the best choice based on the given fit measure (e.g. highest likelihood ratio test statistic, lowest BIC)
+#' - \code{oneSE}: the solution that defined the most parsimonious model within 1 standard error from the best one. 
+#' When both the number of components and the threshold parameter are cross-validated, the 1-standard error rule first finds the candidate alternative solutions that have the same or more parsimonious threshold values, and then, within those, it finds the candidate alternative solutions with the smallest number of components. 
+#' This decision is guided by the desire to prioritize a sparse solution where only important predictors are used to compute the principal components that are used.
+#' Selecting fewer components to the expense of a larger number of (potentially noise) predictors would be suboptimal.
+#' Therefore we favored an implementation that can return more components, but that guarantees a higher quality of the components for the predictive task.
+#' However, this solution might select more components than the original best choice.
 #' @return A list of two numeric vectors:
-#' - \code{default}: coordinates for \code{scor} defining the default solution (min or max value of the fit measure, depending on the type of \code{fit_measure})
-#' - \code{oneSE}: coordinates for \code{scor} defining the solution based on the one standard error rule
+#' - \code{default}: numeric vector of length 2 that reports the coordinates in \code{scor} defining the default solution.
+#' - \code{oneSE}: numeric vector of length 2 that reports the coordinates for \code{scor} defining the solution based on the one standard error rule
 #' @author Edoardo Costantini, 2023
 #' @examples 
 #' # Score matrices
@@ -68,6 +76,12 @@ cv_choose <- function(scor, scor_lwr, scor_upr, K, fit_measure) {
 
     # Are there such solutions?
     if (nrow(candidates) >= 1) {
+        # # Compute differences of the candidate alternative solutions from the bounds
+        # distance_from_bound <- abs(candidates[, "values"] - choice)
+
+        # # Chose the worst fitting solution (closest to the bound)
+        # candidates[which.max(distance_from_bound), ]
+
         # Select the solutions with highest threshold (smallest number of predictors)
         candidates <- candidates[candidates[, "col"] == max(candidates[, "col"]), , drop = FALSE]
 

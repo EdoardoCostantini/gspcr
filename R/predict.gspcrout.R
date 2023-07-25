7c2b65809262dc7027a2cf75a2303166fa5d8a16
Edo <38205413+EdoardoCostantini@users.noreply.github.com>
2023-07-25 11:02:49 +0200

    fix: project pcamix with same categorical value

--- R/predict.gspcrout.R
+++ R/predict.gspcrout.R
@@ -37,11 +37,81 @@ predict.gspcrout <- function(object, newdata = NULL, ...) {
         x_quali <- NULL
     }
 
-    # Project new newdata on the PC space
-    x_PC <- stats::predict(
-        object$pca$pcamix,
-        X.quanti = x_quanti,
-        X.quali = x_quali
+    # Project new data on the PC space
+    x_PC <- tryCatch(
+        {
+            # Normal behavior
+            x_PC <- stats::predict(
+                object$pca$pcamix,
+                X.quanti = x_quanti,
+                X.quali = x_quali
+            )
+            # TODO: The CRAN version of PCAmixdata currently has a bug where if a
+            # new dataset is provided with a categorical column having all the 
+            # same values then new projections are not returned. 
+            # This is undesirable. 
+            # Currently, I handle the error here because the package authors are
+            # aware of the bug. They have actually fixed it in the github version.
+            # However, my package relies on the cran version.
+            # Therefore, as of right now I need the workaround implemented in 
+            # this tryCatch().
+            # Whenever the PCAmixdata package maintainers update the CRAN version
+            # I can get rid of this and just keep the simple normal predict behavior.
+        },
+        error = function(err) {
+            # Error handler message
+            print(
+                paste0(
+                    "The following error occurred when trying to project the new data on the PC axis: ",
+                    "\"",
+                    err,
+                    "\"",
+                    ". Do not worry, this was solved by augmenting the data with an extra row before projecting it, and then getting rid of the extra row before using the projected scores in the subsequent step. If you still get an error after this, then something else went wrong."
+                ),
+                quote = FALSE
+            )
+
+            # identify which variables have constant values
+            contant_values <- sapply(x_quali, function(j) {
+                length(unique(j)) == 1
+            })
+
+            # Create a fake data row
+            x_quali_newraw <- x_quali[1, ]
+            x_quanti_newraw <- x_quanti[1, ]
+
+            # Add a different value for every variable impacted
+            for (j in which(contant_values)) {
+                # Extract the levels of the factor
+                lvls <- levels(x_quali[, j])
+
+                # identify the single used level
+                used_lvl <- unique(x_quali[, j])
+
+                # identify the first unused level (any other would do)
+                unused_lvl <- lvls[!levels(x_quali[, j]) %in% used_lvl][1]
+
+                # Replace it in the fake row
+                x_quali_newraw[, j] <- unused_lvl
+            }
+
+            # Append the extra row to the new data
+            x_quali <- rbind(x_quali, EXTRA = x_quali_newraw)
+            x_quanti <- rbind(x_quanti, EXTRA = x_quanti_newraw)
+
+            # Run PCAmix on the augmented data
+            x_PC <- stats::predict(
+                object$pca$pcamix,
+                X.quanti = x_quanti,
+                X.quali = x_quali
+            )
+
+            # Remove the extra row, if there was a problem with constant values
+            x_PC <- x_PC[-nrow(x_PC), , drop = FALSE]
+
+            # Return the scores
+            return(x_PC)
+        }
     )
 
     # Assemble data
--- tests/testthat/test-predict.gspcrout.R
+++ tests/testthat/test-predict.gspcrout.R
@@ -177,3 +177,56 @@ testthat::expect_true(all(0 < y_hat & y_hat < 1))
 
 # Test the number of categories is right
 testthat::expect_true(ncol(y_hat) == nlevels(GSPCRexdata$y$cat))
+
+# Predict new data with a constant categorical variable ------------------------
+# Note: See predict.gspcrout for details on why this test is needed.
+
+# Make a new dataset with one categorical variable having a single value
+test_contants <- GSPCRexdata$X$mix[-train, ][1:4, ]
+
+# Make a new dataset with no categorical variable having a single value
+test_no_contast <- GSPCRexdata$X$mix[-train, ]
+
+# Use the functions with a given method
+out_cat <- cv_gspcr(
+    dv = GSPCRexdata$y$cont[train],
+    ivs = GSPCRexdata$X$mix[train, ],
+    fam = "gaussian",
+    nthrs = 5,
+    npcs_range = 1:5,
+    K = 3,
+    fit_measure = "LRT",
+    thrs = "PR2",
+    min_features = 1,
+    max_features = ncol(GSPCRexdata$X$cont),
+    oneSE = TRUE
+)
+
+# Estimate GSPCR
+gspcr_est <- est_gspcr(
+    dv = GSPCRexdata$y$cont[train],
+    ivs = GSPCRexdata$X$mix[train, ],
+    fam = "gaussian",
+    ndim = out_cat$sol_table[1, "Q"],
+    active_set = out_cat$pred_map[, out_cat$sol_table[1, "thr_number"]]
+)
+
+# Predict new data with constants
+y_hat_constants <- predict(
+    object = gspcr_est,
+    newdata = test_contants
+)
+
+# Predict new data without constants
+y_hat_no_constants <- predict(
+    object = gspcr_est,
+    newdata = test_no_contast
+)
+
+# Test the same values are predicted for the same observations
+testthat::expect_true(
+    object = all.equal(
+        y_hat_constants[names(y_hat_constants)],
+        y_hat_no_constants[names(y_hat_constants)]
+    )
+)

082ba9034bd2cef4fbc25eab14e1c4195d4a6f39
Edo <38205413+EdoardoCostantini@users.noreply.github.com>
2023-07-27 15:21:17 +0200

    feat: single object to est_gspcr option

--- R/est_gspcr.R
+++ R/est_gspcr.R
@@ -2,13 +2,15 @@
 #'
 #' Estimate SPCA on the data given chosen parameter values
 #'
+#' @param object \code{gspcrcv} object resulting from the call of \code{cv_gspcr()}. If this is specified, then every other argument can be left blank.
 #' @param dv numeric vector or factor of dependent variable values
 #' @param ivs \eqn{n \times p} data.frame of independent variables (factors allowed)
 #' @param fam character vector of length 1 storing the description of the error distribution and link function to be used in the model
-#' @param active_set logical vector indicating which predictor should be used
+#' @param active_set names of the columns of ivs to be used as predictors
 #' @param ndim numeric vector defining the number of principal components to be used (2 or more)
 #' @details
 #' After deciding on the number of components and the active set, this estimates the GSPCR model.
+#' This function can be used by specifying the object argument or by filling in custom values for every argument. If both the object and any other argument are specified, then the argument values will be prioritized.
 #' @return Description of function output
 #' @author Edoardo Costantini, 2023
 #' @references
@@ -16,9 +18,19 @@
 #' Bair, E., Hastie, T., Paul, D., & Tibshirani, R. (2006). Prediction by supervised principal components. Journal of the American Statistical Association, 101(473), 119-137.
 #'
 #' @export
-est_gspcr <- function(dv, ivs, fam, active_set, ndim) {
+est_gspcr <- function(object = NULL, dv, ivs, fam, active_set, ndim) {
+
+    # Process object if not null
+    if(!is.null(object)){
+        dv = object$gspcr_call$dv
+        ivs = object$gspcr_call$ivs
+        fam = object$gspcr_call$fam
+        active_set = names(object$pred_map[, object$sol_table[1, "thr_number"]])
+        ndim = object$sol_table[1, "Q"]
+    }
+
     # Reduced data matrix (active set)
-    ivs_active <- ivs[, active_set]
+    ivs_active <- ivs[, active_set, drop = FALSE]
 
     # Compute the first nidm PC of the reduced data matrix
     PCA_out <- pca_mix(

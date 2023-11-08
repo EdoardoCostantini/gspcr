1aa5335bca21d36099cc568368ea8b6f501ded7b
Edo <38205413+EdoardoCostantini@users.noreply.github.com>
2023-11-08 16:28:25 +0100

    feat: build vignettes in bulk

--- vignettes/rebuild-vignettes.R
+++ vignettes/rebuild-vignettes.R
@@ -10,26 +10,29 @@ old_wd <- getwd()
 
 # Define vignette names
 vignettes <- c(
-    "main-features",
     "vignette-1-example-analysis",
     "vignette-2-gspcr-arguments",
     "vignette-3-alternatives"
 )
 
 # Decide which vignette to compile
-vignette_target <- vignettes[4]
+vignette_target <- vignettes#[1]
 
 # Move to the vignette forlder to simplify figure/ management
 setwd("vignettes/")
 
 # Compile the vignettes that takes a lot of time
-knitr::knit(
-    input = paste0(vignette_target, ".Rmd.orig"),
-    output = paste0(vignette_target, ".Rmd")
-)
+lapply(vignette_target, function(X){
+    knitr::knit(
+        input = paste0(X, ".Rmd.orig"),
+        output = paste0(X, ".Rmd")
+    )
+})
 
 # Revert to original working directory
 setwd(old_wd)
 
 # Compile the vignettes
-devtools::build_rmd(paste0("vignettes/", vignette_target, ".Rmd"))
+lapply(vignette_target, function(X) {
+    devtools::build_rmd(paste0("vignettes/", X, ".Rmd"))
+})

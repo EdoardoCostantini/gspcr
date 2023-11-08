81d48c70c91db583e8ad2364394dca9dbba627e0
Edo <38205413+EdoardoCostantini@users.noreply.github.com>
2023-11-08 16:13:45 +0100

    format: split main-features vignette into multiple

--- vignettes/rebuild-vignettes.R
+++ vignettes/rebuild-vignettes.R
@@ -5,17 +5,31 @@
 # Modified:  2023-11-08
 # Notes:     see https://www.kloppenborg.ca/2021/06/long-running-vignettes/
 
-# # Store current wd
+# Store current wd
 old_wd <- getwd()
 
-# # Move to the vignette forlder to simplify figure/ management
+# Define vignette names
+vignettes <- c(
+    "main-features",
+    "vignette-1-example-analysis",
+    "vignette-2-gspcr-arguments",
+    "vignette-3-alternatives"
+)
+
+# Decide which vignette to compile
+vignette_target <- vignettes[4]
+
+# Move to the vignette forlder to simplify figure/ management
 setwd("vignettes/")
 
 # Compile the vignettes that takes a lot of time
-knitr::knit("main-features.Rmd.orig", output = "main-features.Rmd")
+knitr::knit(
+    input = paste0(vignette_target, ".Rmd.orig"),
+    output = paste0(vignette_target, ".Rmd")
+)
 
 # Revert to original working directory
 setwd(old_wd)
 
 # Compile the vignettes
-devtools::build_rmd("vignettes/main-features.Rmd")
+devtools::build_rmd(paste0("vignettes/", vignette_target, ".Rmd"))
